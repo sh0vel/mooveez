a1c723a1686a215d5cd2819de9034d2c50c6c67d
shovonsemail@yahoo.com <shovonsemail@gmail.com>
2016-06-18 17:55:11 -0400

    added rate button

--- app/src/main/java/com/app/shovonh/mooveez/MainActivity.java
+++ app/src/main/java/com/app/shovonh/mooveez/MainActivity.java
@@ -8,11 +8,14 @@ import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.Uri;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -20,7 +23,6 @@ import android.widget.FrameLayout;
 
 import com.app.shovonh.mooveez.Objs.MovieObj;
 import com.app.shovonh.mooveez.Receivers.MonthlyNotifications;
-import com.app.shovonh.mooveez.data.AlarmDBHelper;
 
 import org.parceler.Parcels;
 
@@ -31,13 +33,11 @@ import hirondelle.date4j.DateTime;
 public class MainActivity extends AppCompatActivity implements MovieRecyclerFrag.Callback {
     private static final String LOG_TAG = MainActivity.class.getSimpleName();
 
-    public static AlarmDBHelper dbHelper;
-
+    SharedPreferences mPrefs;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
 
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -48,26 +48,12 @@ public class MainActivity extends AppCompatActivity implements MovieRecyclerFrag
         toolbar.setTitleTextColor(Color.WHITE);
 
         FrameLayout card = (FrameLayout) findViewById(R.id.no_network_view);
+        mPrefs = PreferenceManager.getDefaultSharedPreferences(this);
 
 
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        String s = prefs.getString("isReceiverSet", "no");
-        if (s.equals("no")) {
-            DateTime d = DateTime.now(TimeZone.getDefault());
-            if (d.getDay() != 1) {
-                Intent i = new Intent(this, MonthlyNotifications.class);
-                PendingIntent pi = PendingIntent.getBroadcast(this, d.getDay(), i, 0);
+        setReciever();
+        rateSnackBar(findViewById(R.id.container));
 
-                String firstOfNext = d.getEndOfMonth().plusDays(1).format("YYYY-MM-DD");
-                DateTime dt = new DateTime(firstOfNext + " 06:00:00");
-
-                AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
-                am.set(AlarmManager.RTC_WAKEUP, dt.getMilliseconds(TimeZone.getDefault()), pi);
-
-                SharedPreferences.Editor editor = prefs.edit();
-                editor.putString("isReceiverSet", "yes");
-            }
-        }
 
         if (hasConnection(this)) {
             card.setVisibility(View.GONE);
@@ -90,7 +76,7 @@ public class MainActivity extends AppCompatActivity implements MovieRecyclerFrag
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        //getMenuInflater().inflate(R.menu.menu_main, menu);
+        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -102,11 +88,76 @@ public class MainActivity extends AppCompatActivity implements MovieRecyclerFrag
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_rate) {
+            rateApp();
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setReciever() {
+        String s = mPrefs.getString("isReceiverSet", "no");
+        if (s.equals("no")) {
+            DateTime d = DateTime.now(TimeZone.getDefault());
+            if (d.getDay() != 1) {
+                Intent i = new Intent(this, MonthlyNotifications.class);
+                PendingIntent pi = PendingIntent.getBroadcast(this, d.getDay(), i, 0);
+
+                String firstOfNext = d.getEndOfMonth().plusDays(1).format("YYYY-MM-DD");
+                DateTime dt = new DateTime(firstOfNext + " 06:00:00");
+
+                AlarmManager am = (AlarmManager) this.getSystemService(Context.ALARM_SERVICE);
+                am.set(AlarmManager.RTC_WAKEUP, dt.getMilliseconds(TimeZone.getDefault()), pi);
+
+                SharedPreferences.Editor editor = mPrefs.edit();
+                editor.putString("isReceiverSet", "yes");
+                editor.apply();
+            }
+        }
+    }
+
+    public void rateSnackBar(View view) {
+
+        if (mPrefs.getString("hasRated", "no").equals("no")) {
+            final SharedPreferences.Editor editor = mPrefs.edit();
+            int count = mPrefs.getInt("count", 0);
+            Log.v(LOG_TAG, "secound " + count);
+            if (count >= 3) {
+                View.OnClickListener rateButton = new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        rateApp();
+                    }
+                };
+                Snackbar sb = Snackbar.make(view, "Rate this app?", Snackbar.LENGTH_INDEFINITE)
+                        .setAction("RATE", rateButton)
+                        .setCallback(new Snackbar.Callback() {
+                            @Override
+                            public void onDismissed(Snackbar snackbar, int event) {
+                                super.onDismissed(snackbar, event);
+                                editor.putInt("count", 0).apply();
+                            }
+                        })
+                        .setActionTextColor(getResources().getColor(R.color.colorPrimary));
+                sb.show();
+            } else {
+                count++;
+                editor.putInt("count", count).apply();
+            }
+        }
+    }
+
+    public void rateApp() {
+        SharedPreferences.Editor editor = mPrefs.edit();
+        editor.putString("hasRated", "yes");
+        editor.apply();
+        final String appPackageName = getPackageName();
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException anfe) {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
     }
 
 
